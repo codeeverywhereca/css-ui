626a82345f7ea9b9c27d7a044ef1f0bb44ac39f5
codeeverywhereca <codeeverywhereca@users.noreply.github.com>
2015-07-18 16:56:29 -0400

    Create build.php

---
+++ build.php
@@ -0,0 +1,31 @@
+<?php
+
+$files = array(
+	"components/buttons.html",
+	"components/formatting.html",
+	"components/grid.html",
+	"components/messages.html",
+	"components/tables.html"
+);
+
+$header = "/*
+* CSS UI v1.6b3 (Jul 11 2015)
+* http://css-ui.com
+*
+* Copyright 2015, http://codeeverywhere.ca
+* Licensed under the MIT license.
+*/
+";
+
+file_put_contents("ui.min.css", $header);
+
+foreach( $files as $filename )
+{
+	$data = file_get_contents($filename);
+	$data = preg_replace('/\s+/', " ", $data);	
+	preg_match("/\<style\>(.*)\<\/style\>/", $data, $css);
+	$css = $css[1];
+	file_put_contents("ui.min.css", $css, FILE_APPEND);
+}
+
+?>
