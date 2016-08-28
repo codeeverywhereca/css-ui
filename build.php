563eef7f3377abd2848b1fcdb44d9ef6158f5332
codeeverywhereca <codeeverywhereca@users.noreply.github.com>
2016-08-27 22:30:54 -0400

    Update build.php

--- build.php
+++ build.php
@@ -1,5 +1,4 @@
 <?php
-
 $files = array(
 	"components/buttons.html",
 	"components/formatting.html",
@@ -10,23 +9,20 @@ $files = array(
 );
 
 $header = "/*
-* CSS UI v1.6 (Aug 2016)
+* CSS UI v1.6 (Aug 27 2016)
 * http://css-ui.com
 *
-* Copyright 2015, http://codeeverywhere.ca
+* Copyright 2016, http://codeeverywhere.ca
 * Licensed under the MIT license.
-*/
-";
+*/";
 
 file_put_contents("ui.min.css", $header);
 
-foreach( $files as $filename )
-{
+foreach( $files as $filename ) {
 	$data = file_get_contents($filename);
 	$data = preg_replace('/\s+/', " ", $data);	
 	preg_match("/\<style\>(.*)\<\/style\>/", $data, $css);
 	$css = $css[1];
 	file_put_contents("ui.min.css", $css, FILE_APPEND);
 }
-
 ?>
