48cb3d15e4e6037be8e7ec9bbe81d4be50b3cbd2
codeeverywhereca <codeeverywhereca@users.noreply.github.com>
2016-08-27 22:26:50 -0400

    Update build.php

--- build.php
+++ build.php
@@ -5,11 +5,12 @@ $files = array(
 	"components/formatting.html",
 	"components/grid.html",
 	"components/messages.html",
-	"components/tables.html"
+	"components/tables.html",
+	"components/tabs.html"
 );
 
 $header = "/*
-* CSS UI v1.6b5 (Oct 12 2015)
+* CSS UI v1.6 (Aug 2016)
 * http://css-ui.com
 *
 * Copyright 2015, http://codeeverywhere.ca
